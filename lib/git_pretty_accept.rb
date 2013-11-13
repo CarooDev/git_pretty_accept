cc638bd33b784172cac03c1241a9edc7d92921da
George Mendoza <gsmendoza@gmail.com>
2013-11-13 11:11:43 +0800

    Fix: app cannot find VERSION.
    Solution: require version before app.

--- lib/git_pretty_accept.rb
+++ lib/git_pretty_accept.rb
@@ -2,8 +2,8 @@ require 'git'
 require 'optparse'
 require 'methadone'
 
-require "git_pretty_accept/app"
 require "git_pretty_accept/version"
+require "git_pretty_accept/app"
 
 module GitPrettyAccept
   # Your code goes here...
